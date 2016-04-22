1ed955ce1dfe8ebd3ee8e0208fa9b92dba1c310e
Julia Herron <julia.herron@gmail.com>
2016-04-22 07:57:09 -0700

    better file structure

--- background.js
+++
@@ -1,9 +0,0 @@
-// background script that has access to Chrome API
-// focuses on the active tab 
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    var url = activeTab.url;
-    chrome.tabs.sendMessage(url)
-  })
-})
